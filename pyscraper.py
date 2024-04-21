eafd71fb6ff45d1cb0eb08c74117f67bd5a52efd
Javier Hernández Sánchez <javierhersan1999@gmail.com>
2024-04-21 12:13:05 +0200

    feature(pyscraper): Click buttons, fill in inputs, wait events, and parse html

--- pyscraper.py
+++ pyscraper.py
@@ -1,6 +1,16 @@
 from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False, slow_mo=50)
+    browser = p.chromium.launch(headless=False, slow_mo=50) # headless to see the browser
     page = browser.new_page()
     page.goto("https://maps.app.goo.gl/9zgDBvgzVm8gvRw27?g_st=ic")
+    page.fill("input#input-username", "admin")
+    page.fill("input#input-password", "pass")
+    page.click("button[type=submit]")
+    page.is_visible("div.tile-body")
+    html = page.inner_html("#id")
+    soup = BeautifulSoup(html, "html.parser")
+
+    total = soup.find_all("h2", {"class": "pull-right"}).text
+    print(f'total_orders = {total}')
